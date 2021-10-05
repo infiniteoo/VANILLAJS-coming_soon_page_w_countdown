61ac7f6a257ca6faf0d60596ac2ad79cb922f343
Troy Dorman <troydorman@gmail.com>
2021-10-05 09:47:24 -0600

    created 1 second interval.  defined now in ms.  determined how many ms left between now and launch date

--- js/main.js
+++ js/main.js
@@ -1,6 +1,17 @@
 const countdown = document.querySelector(".countdown");
 
-// set launch date
+// set launch date (ms)
 const launchDate = new Date("Jan 1, 2022 13:00:00").getTime();
 
 console.log(launchDate);
+
+// update every section
+const interval = setInterval(() => {
+  // get today's date and time (in ms)
+  const now = new Date().getTime();
+
+  // distance from now to launch date
+  const distance = launchDate - now;
+
+  console.log(distance);
+}, 1000);
