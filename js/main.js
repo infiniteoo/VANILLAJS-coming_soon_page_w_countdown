35c589abcf34b2c8b3790791e3fd3a6ea1a1287e
Troy Dorman <troydorman@gmail.com>
2021-10-05 09:49:30 -0600

    defined the days, hours, mins and seconds between now and launch date

--- js/main.js
+++ js/main.js
@@ -13,5 +13,13 @@ const interval = setInterval(() => {
   // distance from now to launch date
   const distance = launchDate - now;
 
-  console.log(distance);
+  // time calculations
+  const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  const hours = Math.floor(
+    (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+  );
+  const mins = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  console.log(days, hours, mins, seconds);
 }, 1000);
